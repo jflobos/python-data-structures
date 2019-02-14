c8372de1bbb4d4bf97752e3b0c1e96f7b0e62602
Juan Francisco Lobos <jflobos@gmail.com>
2019-02-13 22:58:47 -0300

    Add stack data-structure

---
+++ data-structures/stack/test_Stack.py
@@ -0,0 +1,12 @@
+from .Stack import Stack
+
+def test_push():
+    stack = Stack()
+    stack.push(2)
+    assert stack.first() == 2
+
+def test_pop():
+    stack = Stack()
+    stack.push(2)
+    assert stack.pop() == 2
+    assert stack.first() is None
