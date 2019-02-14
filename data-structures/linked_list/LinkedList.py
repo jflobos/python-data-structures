87b2e92b51ca27c431e8053fb5ac9e9e5591dd17
Juan Francisco Lobos <jflobos@gmail.com>
2019-02-13 22:42:37 -0300

    Add pytest test suite for linked-list

--- data-structures/linked_list/LinkedList.py
+++ data-structures/linked_list/LinkedList.py
@@ -3,11 +3,23 @@ class LinkedList:
         self.next = None
         self.value = value
 
-    def insert(self, value):
-        if self.next != None:
-            self.next.insert(value)
+    def append(self, value):
+        if self.value is None:
+            self.value = value
         else:
-            self.next = LinkedList(value)
+            if self.next is not None:
+                self.next.append(value)
+            else:
+                self.next = LinkedList(value)
+    
+    def first(self):
+        return self.value
+    
+    def pop(self):
+        aux = self.value
+        self.__replace_by_next()
+        return aux
+
     
     def find(self, value):
         if self.value == value:
@@ -19,16 +31,10 @@ class LinkedList:
 
     def remove(self, value):
         if self.value == value:
-            if self.next is not None:
-                self.value = self.next.value
-                self.next = self.next.next
-            else:
-                self.value = None
-                self.next = None
+            self.__replace_by_next()
         elif self.next is not None:
             return self.next.remove(value)
        
-    
     def get_values(self, values = None):
         if values is None:
             values = []
@@ -44,10 +50,16 @@ class LinkedList:
         if self.next is not None:
             self.next.print_values()
         
-    
     def count(self, count = 0):
         if self.value is not None:
             count += 1
         if self.next is not None:
             return self.next.count(count)
         return count
+    
+    def __replace_by_next(self):
+        if self.next is not None:
+            self.value = self.next.value
+            self.next = self.next.next
+        else:
+            self.value = None
